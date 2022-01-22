63258eeadb72fe26104d019cf1d4fb28fef07d9e
JupiterPi <jupiterpi@outlook.de>
2022-01-22 18:44:40 +0100

    enderchest command

---
+++ src/main/java/de/jupiterpi/mc/edcaria/EnderchestCommand.java
@@ -0,0 +1,21 @@
+package de.jupiterpi.mc.edcaria;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class EnderchestCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Only players can use this command!");
+            return true;
+        }
+
+        player.openInventory(player.getEnderChest());
+
+        return true;
+    }
+}
